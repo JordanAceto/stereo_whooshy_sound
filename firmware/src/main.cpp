da946f92f84ee4e22aac7b75c9ca6bbaa9c06f1f
Jordan Aceto <ja532@cornell.edu>
2022-05-18 13:35:48 -0400

    Update main, basic app now working

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,50 +1,199 @@
 
-#include "BSP_ADC1.hpp"
 #include "BSP_system_init.hpp"
 #include "BSP_TIM2.hpp"
 #include "LFO.hpp"
 #include "lookup_tables.hpp"
 #include "MCP4822.hpp"
-#include "stm32l031xx.h"
+#include "ui_controls.hpp"
+
+/* the two independent LFO objects */
+LFO lfo_A(BSP::tim2::FREQUENCY_Hz);
+LFO lfo_B(BSP::tim2::FREQUENCY_Hz);
+
+/* the user interface components, pots and switches */
+UI ui{};
+
+/**
+ * @brief read the LFO rate controls and update the frequency for LFO A and B
+ */
+void update_lfo_frequency();
+
+/**
+ * @brief read the LFO wave shape controls and update the waveshapes for LFO A and B
+ */
+void update_lfo_wave_scanners();
+
+/**
+ * @brief calculate the final value to write to the DAC for output A
+ *
+ * When the sweep mode is INDEPENDENT or SYNC_TO_A, this is the value of LFO A
+ * scaled by the depth A control
+ *
+ * When the sweep mode is ADD, this is the value of value of LFO A
+ * scaled by the depth A control plus the value of LFO B scaled by
+ * the depth B control
+ *
+ * @return int the value to write to the DAC for output A
+ */
+int calc_lfo_A_val();
+
+/**
+ * @brief calculate the final value to write to the DAC for output B
+ *
+ * When the sweep mode is INDEPENDENT, this is the value of LFO B
+ * scaled by the depth B control, or -(LFO B) if the invert B switch is on
+ *
+ * When the sweep mode is SYNC_TO_A, this is the value of LFO A scaled by the
+ * depth B control, or -(LFO A) if the invert B switch is on
+ *
+ * When the sweep mode is ADD, this is the value of LFO A
+ * scaled by the depth A control plus the value of LFO B scaled by
+ * the depth B control, or (LFO A) - (LFO B) if the invert B switch is on
+ *
+ * @return int
+ */
+int calc_lfo_B_val();
+
+/**
+ * @brief write the values of LFO A and B to the SPI DAC
+ */
+void write_to_dacs();
 
 int main(void)
 {
     BSP::system_init::init();
 
-    LFO lfo_A(BSP::tim2::FREQUENCY_Hz);
-    LFO lfo_B(BSP::tim2::FREQUENCY_Hz);
-
-    while(1)
+    while (1)
     {
         if (BSP::tim2::getTimeout())
         {
-            GPIOA->BSRR |= GPIO_BSRR_BS_6; // temporary test code to time execution
-
             lfo_A.tick();
             lfo_B.tick();
 
-            // update the LFO frequency
-            const int freq_lut_idx_ui10 = BSP::adc1::getInput(BSP::adc1::LFO_A_FREQ) >> 6;
-            lfo_A.setInput(LFO::FREQ_mHz, Lookup_Tables::EXPO_MAPPING_LUT_mHz[freq_lut_idx_ui10]);
-
-            // update the LFO wave scanner
-            const int wave_scan_ui16 = BSP::adc1::getInput(BSP::adc1::LFO_A_SHAPE);
-            lfo_A.setInput(LFO::WAVE_SCAN, wave_scan_ui16);
-
-            // attenuate the LFO by the level control and center it in range [0, 0x0FFF] for the DAC
-            int scaled_lfo = lfo_A.getOutput(LFO::CROSSFADED);
-            scaled_lfo *= BSP::adc1::getInput(BSP::adc1::LFO_A_LEVEL);
-            scaled_lfo /= (BSP::adc1::FULL_SCALE + 1); // dividing by an exact power of 2 allows for compiler optimizations
-            scaled_lfo += LFO::OUTPUT_MAX_VAL;
-
-            MCP4822::write(scaled_lfo, MCP4822::Channel::A);
-
-            MCP4822::write(lfo_B.getOutput(LFO::SINE), MCP4822::Channel::B);
-
-            GPIOA->BSRR |= GPIO_BSRR_BR_6; // temporary test code to time execution
+            update_lfo_frequency();
+            update_lfo_wave_scanners();
+            write_to_dacs();
         }
     }
 
     // never reached
     return 0;
+}
+
+void update_lfo_frequency()
+{
+    auto freq_lut_idx_ui10 = ui.get_control_signal_val(UI::LFO_A_FREQ);
+    lfo_A.setInput(LFO::FREQ_mHz, Lookup_Tables::EXPO_MAPPING_LUT_mHz[freq_lut_idx_ui10]);
+
+    freq_lut_idx_ui10 = ui.get_control_signal_val(UI::LFO_B_FREQ);
+    lfo_B.setInput(LFO::FREQ_mHz, Lookup_Tables::EXPO_MAPPING_LUT_mHz[freq_lut_idx_ui10]);
+}
+
+void update_lfo_wave_scanners()
+{
+    lfo_A.setInput(
+        LFO::WAVE_SCAN,
+        ui.get_control_signal_val(UI::LFO_A_SHAPE));
+
+    lfo_B.setInput(
+        LFO::WAVE_SCAN,
+        ui.get_control_signal_val(UI::LFO_B_SHAPE));
+}
+
+int calc_lfo_A_val()
+{
+    int retval;
+
+    switch (ui.get_sweep_mode())
+    {
+    // in INDEPENDENT and SYNC_TO_A modes just take the val of LFO A
+    case UI::sweep_mode::INDEPENDENT:
+    case UI::sweep_mode::SYNC_TO_A:
+    {
+        retval = ui.scale_bipolar_signal(
+            lfo_A.getOutput(LFO::CROSSFADED),
+            ui.get_control_signal_val(UI::LFO_A_LEVEL));
+        break;
+    }
+    // in ADD mode, take (LFO A) + (LFO B)
+    case UI::sweep_mode::ADD:
+    {
+        auto lfo_A_contrib = ui.scale_bipolar_signal(
+            lfo_A.getOutput(LFO::CROSSFADED),
+            ui.get_control_signal_val(UI::LFO_A_LEVEL));
+
+        auto lfo_B_contrib = ui.scale_bipolar_signal(
+            lfo_B.getOutput(LFO::CROSSFADED),
+            ui.get_control_signal_val(UI::LFO_B_LEVEL));
+
+        retval = lfo_A_contrib + lfo_B_contrib;
+        break;
+    }
+    }
+
+    return ui.condition_bipolar_signal_for_DAC(retval);
+}
+
+int calc_lfo_B_val()
+{
+    const auto lfo_B_mode = ui.get_lfo_b_mode();
+
+    int retval;
+
+    switch (ui.get_sweep_mode())
+    {
+    // in INDEPENDENT mode just take the val of LFO B, optionally inverted
+    case UI::sweep_mode::INDEPENDENT:
+    {
+        retval = ui.scale_bipolar_signal(
+            lfo_B.getOutput(LFO::CROSSFADED),
+            ui.get_control_signal_val(UI::LFO_B_LEVEL));
+
+        if (lfo_B_mode == UI::lfo_b_mode::INVERT)
+        {
+            retval = -retval;
+        }
+        break;
+    }
+    // in SYNC_TO_A modes just take the val of LFO A, optionally inverted
+    case UI::sweep_mode::SYNC_TO_A:
+    {
+        retval = ui.scale_bipolar_signal(
+            lfo_A.getOutput(LFO::CROSSFADED),
+            ui.get_control_signal_val(UI::LFO_B_LEVEL));
+
+        if (lfo_B_mode == UI::lfo_b_mode::INVERT)
+        {
+            retval = -retval;
+        }
+        break;
+    }
+    // in ADD mode, take (LFO A) + (LFO B), with LFO B optionally inverted
+    case UI::sweep_mode::ADD:
+    {
+        auto lfo_A_contrib = ui.scale_bipolar_signal(
+            lfo_A.getOutput(LFO::CROSSFADED),
+            ui.get_control_signal_val(UI::LFO_A_LEVEL));
+
+        auto lfo_B_contrib = ui.scale_bipolar_signal(
+            lfo_B.getOutput(LFO::CROSSFADED),
+            ui.get_control_signal_val(UI::LFO_B_LEVEL));
+
+        if (ui.get_lfo_b_mode() == UI::lfo_b_mode::INVERT)
+        {
+            lfo_B_contrib = -lfo_B_contrib;
+        }
+
+        retval = lfo_A_contrib + lfo_B_contrib;
+        break;
+    }
+    }
+
+    return ui.condition_bipolar_signal_for_DAC(retval);
+}
+
+void write_to_dacs()
+{
+    MCP4822::write(calc_lfo_A_val(), MCP4822::Channel::A);
+    MCP4822::write(calc_lfo_B_val(), MCP4822::Channel::B);
 }
